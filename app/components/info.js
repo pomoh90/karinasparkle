62f7ee62125ce110cbd291c146564fe53f429a80
pomoh90 <87230076+pomoh90@users.noreply.github.com>
2025-07-24 22:57:11 +0200

    Update info.js

--- app/components/info.js
+++ app/components/info.js
@@ -2,7 +2,7 @@
 export const contactInfo = {
     phone: '+1 (646) 551-3931',
     email: 'Karinasparklenyc@yahoo.com',
-    ter: '/',
+    ter: 'https://theeroticreview.com/reviews/show.asp?id=406748',
     terShort: 'The Erotic Review',
 
   };
